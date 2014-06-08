ff6338b78794891511b71832e43e58c68601bcc4
Sarjono Mukti Aji <me@simukti.net>
2014-06-08 16:01:48 +0700

    add GoogleAuth and EdpModuleLayouts

--- config/application.config.php
+++ config/application.config.php
@@ -2,12 +2,15 @@
 return array(
     'modules' => array(
         'Application',
-        'Simukti'
+        'Simukti',
+        'GoogleAuth',
+        'EdpModuleLayouts',
     ),
 
     'module_listener_options' => array(
         'module_paths' => array(
             './module',
+            './vendor',
         ),
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
