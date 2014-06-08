ec03e3d211a45e40b52f456b4472c1955d2d5b20
Sarjono Mukti Aji <me@simukti.net>
2014-06-08 17:35:38 +0700

    change to standard autoloader

--- module/Simukti/Module.php
+++ module/Simukti/Module.php
@@ -11,8 +11,10 @@ class Module
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassmapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php'
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
             ),
         );
     }
